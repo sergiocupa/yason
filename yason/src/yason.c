75a81555e9704489f0abc3a47335adc0d6a8ee50
DESKTOP-TLGP5B5\sergio <sergiocupa@outlook.com>
2025-08-29 00:31:03 -0300

    update license

--- yason/src/yason.c
+++ yason/src/yason.c
@@ -1,12 +1,14 @@
 //  MIT License – Modified for Mandatory Attribution
 //  
 //  Copyright(c) 2025 Sergio Paludo
+//
+//  github.com/sergiocupa
 //  
 //  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files, 
 //  to use, copy, modify, merge, publish, distribute, and sublicense the software, including for commercial purposes, provided that:
 //  
-//  01. The original author’s credit is retained in all copies of the source code;
-//  02. The original author’s credit is included in any code generated, derived, or distributed from this software, including templates, libraries, or code - generating scripts.
+//     01. The original author’s credit is retained in all copies of the source code;
+//     02. The original author’s credit is included in any code generated, derived, or distributed from this software, including templates, libraries, or code - generating scripts.
 //  
 //  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED.
 
